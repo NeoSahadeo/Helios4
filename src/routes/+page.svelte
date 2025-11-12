4892b83f94a9889d79f4bc6cb174bf2da40c11da
NeoSahadeo <neosahadeo@protonmail.com>
2025-11-12 14:57:00 +0200

    [feat] Updating UI

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,31 +1,23 @@
 <script lang="ts">
 	import { resolve } from '$app/paths';
-	import Logo from '$lib/assets/helios4logo.svg';
 	import GithubButton from '$lib/components/GithubButton.svelte';
 	import LoginIcon from '$lib/icons/LoginIcon.svelte';
 	import '$lib/styles/misc.css';
 </script>
 
 <!-- <div class="absolute -top-10 -left-20 h-32 w-32 bg-[var(--clr-primary-a0)] blur-3xl"></div> -->
-<div class="sun-blob absolute -top-10 -left-20"></div>
 <article class="flex h-full w-full flex-col items-center justify-center">
-	<img src={Logo} alt="Helio4" class="w-60" />
-	<div class="mx-5 mb-5">
-		<p class="text-primary max-w-sm pt-4 text-center">
-			A no muss, no fuss password manager platform.
-		</p>
-		<p class="text-primary pt-2 text-right text-xs">
-			- created by <a href="https://github.com/NeoSahadeo/" target="_blank">Neo Sahadeo</a>
-		</p>
+	<div class="-mt-44 mb-20 flex flex-col items-end">
+		<p class="zilla-slab-bold primary-text" style="font-size: 5.5em;">Helios 4</p>
+		<p class="zilla-slab-bold primary-text -mt-6" style="font-size: 1.5em;">by Neo Sahadeo</p>
 	</div>
 	<div class="flex flex-col gap-5">
 		<button
-			class="btn btn-primary"
+			class="primary-background zilla-slab-bold glass-card border-0 px-10 py-3 text-2xl text-yellow-800 transition-all duration-100 hover:scale-105 hover:cursor-pointer"
 			onclick={() => {
 				document.location.href = resolve('/login');
-			}}>Log In <span style="padding-top: 1px;"> <LoginIcon /></span></button
+			}}>Log In</button
 		>
 		<GithubButton />
 	</div>
-	<!-- <p class="font-bold text-[var(--clr-primary-a0)]">Created By Neo Sahadeo</p> -->
 </article>
